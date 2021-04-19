b07c2dd27a1bd6b15da1d3c949e3af1c9a3af278
hunter britten <huntergbritten@gmail.com>
2021-04-19 16:34:32 -0700

    css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 // import Form from 'react-bootstrap/Form';
 // import Container from 'react-bootstrap/Container';
 
@@ -16,33 +17,30 @@ class App extends React.Component {
     e.preventDefault();
     const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
     const res = await axios.get(API);
-    console.log(res.data[0])
+
     const location = res.data[0];
-    console.log('location', location.display_name);
+
     this.setState({ location, });
-
-    // const loc = await axios.get(img_url);
-
-    // const local = loc.data.lat;
   }
+
   render() {
     return (
       <>
-        <form> Where do you want to go? <br>
-        </br>
-          <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
-          <button onClick={this.getLocation}>Explore!</button>
+        <div>
+          <form id="form"> Where do you want to go? <br>
+          </br>
+            <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
+            <button onClick={this.getLocation}>Explore!</button>
+          </form>
 
-        </form>
-
-        {this.state.location.place_id &&
-          <h2>The city is: {this.state.location.display_name}</h2>
-        }
-        {this.state.location.lat &&
-          <h3>{this.state.location.lat}, is the latitude and {this.state.location.lon}, is the longitude. </h3>}
-        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&size=${window.innerWidth}x300&format=jpg&zoom=12`} alt={this.state.location.display_name} />
+          {this.state.location.place_id &&
+            <h2>The city is: {this.state.location.display_name}</h2>
+          }
+          {this.state.location.lat &&
+            <h3>{this.state.location.lat}, is the latitude and {this.state.location.lon}, is the longitude. </h3>}
+          <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&size=${window.innerWidth}x300&format=jpg&zoom=12`} alt={this.state.location.display_name} />
+        </div>
       </>
-
     )
   }
 }
